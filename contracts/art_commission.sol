3fdcf0161db8a4fcf5291542881171184c77051a
alison-at <alisonateske@gmail.com>
2026-04-05 12:59:59 -0400

    reworked skeleton: the buyer initiates the contract, the artist accepts

--- contracts/art_commission.sol
+++ contracts/art_commission.sol
@@ -4,20 +4,31 @@ pragma solidity ^0.8.26;
 import "hardhat/console.sol";
 
 contract ArtCommision {
+    //ALL PRICES IN WEI!!
     address public payable artist;
     address public payable buyer;
     uint256 upfrontPaymentPercent;
     uint256 lastPayamentPercent;
     unit256 insuranceAmount;
     uint256 fullPrice;
+    uint256 numberOfDaysToCompletion;
 
-    enum StateOfProgressP{InitialAgreement, InProgress, WorkCompleted, WorkPayed, Completed, Disputed}
-    StateOfProgress public progressTracker
+    enum State{Proposed, Confirmed, WorkCompleted, WorkPayed, Completed, Disputed}
+    StateOfProgress public progress;
     
-    //The artist creates the work but the buyer must confirm initial agreement to set inProgress? or other way around
-    constructor(uint256 inuranceAmoung, uint256 price, uint256 upfrontPaymentPercent) {
+    constructor(uint256 inuranceAmoung, uint256 price, uint256 upfrontPaymentPercent, uint256 timeframe) {
+        //The buyer will construct the contract
         buyer = payable(msg.sender)
+
+        //require the amount of insurance to be more than .015 ETH in total, about .075 or $15 per party
+        require(insuranceAmount > 7500000000000000, "Insurance too low")
+
         //check that the value sent is the upfront percent of the total price price is 
+        uint256 upfrontPayment = price*(1/upfrontPaymentPercent)
+        require(msg.value == upfrontPayment + insuranceAmount)
+
+        progress = State.Proposed;
+
     }
 
     modifier onlyArtist() {
@@ -32,18 +43,36 @@ contract ArtCommision {
 
     modifier onlyParties() {
         require(msg.sender == buyer | msg.sender == artist, "Not involved party")
+        _;
     }
+
 
     //this confirms the parameters set in the constructor are ok with the other party
-    function buyerConfirm() {
-
+    function artistConfirm() onlyArtist {
+        require(msg.value == insuranceAmount, "Insurance too low")
+        
+        //set the state to confirmed
+        progress = State.Confirmed;
     }
 
-    function vault() {
+    //the artist submits work, IKD how to accept ERC721
+    function acceptArt(address art) {
+        progress = State.WorkCompleted;
+    }
+
+    //the buyer pays for work, work and payment are released
+    function payInFullAndRelease() onlyBuyer {
+
+        progress = State.Complete;
+    }
+
+    //update the trust score of buyer and artist
+    function updateTrustworthiness() {
 
     }
 
     function raiseDispute() onlyParties {
 
+        progress = State.Disputed;
     }
 }
