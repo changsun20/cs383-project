33192356b0a9b8db2088cd97fb45881bd69957de
alison-at <alisonateske@gmail.com>
2026-04-05 11:27:44 -0400

    add details to contract constructor

--- contracts/art_commission.sol
+++ contracts/art_commission.sol
@@ -4,14 +4,20 @@ pragma solidity ^0.8.26;
 import "hardhat/console.sol";
 
 contract ArtCommision {
-    address payable artist;
-    address payable buyer;
+    address public payable artist;
+    address public payable buyer;
     uint256 upfrontPaymentPercent;
     uint256 lastPayamentPercent;
     unit256 insuranceAmount;
+    uint256 fullPrice;
 
-    constructor() {
-
+    enum StateOfProgressP{InitialAgreement, InProgress, WorkCompleted, WorkPayed, Completed, Disputed}
+    StateOfProgress public progressTracker
+    
+    //The artist creates the work but the buyer must confirm initial agreement to set inProgress? or other way around
+    constructor(uint256 inuranceAmoung, uint256 price, uint256 upfrontPaymentPercent) {
+        buyer = payable(msg.sender)
+        //check that the value sent is the upfront percent of the total price price is 
     }
 
     modifier onlyArtist() {
@@ -19,16 +25,25 @@ contract ArtCommision {
         _;
     }
 
-    modifer onlyBuyer() {
+    modifier onlyBuyer() {
         require(msg.sender == buyer, "Not buyer") 
         _;
+    }
+
+    modifier onlyParties() {
+        require(msg.sender == buyer | msg.sender == artist, "Not involved party")
+    }
+
+    //this confirms the parameters set in the constructor are ok with the other party
+    function buyerConfirm() {
+
     }
 
     function vault() {
 
     }
 
-    function raiseDispute() {
+    function raiseDispute() onlyParties {
 
     }
 }
