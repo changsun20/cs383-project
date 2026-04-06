f81e28345dab625faa3bb0d24d5191e4975f86e4
changsun20 <110759360+changsun20@users.noreply.github.com>
2026-04-06 00:42:09 -0400

    fix: fix compile issues in commission

--- contracts/art_commission.sol
+++ contracts/art_commission.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.26;
 
 // Import this file to use console.log
@@ -7,32 +8,38 @@ import "hardhat/console.sol";
 interface IERC721 {
     function safeTransferFrom(address from, address to, uint256 tokenID) external; // safely transfers ERC721 for both EOA and contract addresses
     function approve(address to, uint256 tokenID) external; // required for safeTransfer involving a contract, approves "to" address as recipient
+    function ownerOf(uint256 tokenID) external view returns (address);
 }
 
 contract ArtCommission {
     //ALL PRICES IN WEI!!
-    address public payable artist;
-    address public payable buyer;
+    address public artist;
+    address public buyer;
     uint256 upfrontPaymentPercent;
     uint256 lastPaymentPercent;
-    unit256 insuranceAmount;
+    uint256 insuranceAmount;
     uint256 fullPrice;
     uint256 numberOfDaysToCompletion;
     IERC721 artwork;
 
     enum State{Proposed, Confirmed, WorkCompleted, WorkPayed, Completed, Disputed}
-    StateOfProgress public progress;
+    State public progress;
     
     // buyer payment amount is locked in the contract upon deployment
-    constructor(uint256 inuranceAmount, uint256 price, uint256 upfrontPaymentPercent, uint256 timeframe) payable {
+    constructor(uint256 _insuranceAmount, uint256 price, uint256 _upfrontPaymentPercent, uint256 timeframe) payable {
         //The buyer will construct the contract
         buyer = payable(msg.sender);
 
         //require the amount of insurance to be more than .015 ETH in total, about .075 or $15 per party
-        require(insuranceAmount > 7500000000000000, "Insurance too low");
+        require(_insuranceAmount > 7500000000000000, "Insurance too low");
+        insuranceAmount = _insuranceAmount;
+        upfrontPaymentPercent = _upfrontPaymentPercent;
+        fullPrice = price;
+        numberOfDaysToCompletion = timeframe;
+        lastPaymentPercent = 100 - upfrontPaymentPercent;
 
         //check that the value sent is the upfront percent of the total price price is 
-        uint256 upfrontPayment = price*(1/upfrontPaymentPercent);
+        uint256 upfrontPayment = price * upfrontPaymentPercent / 100;
         require(msg.value == upfrontPayment + insuranceAmount);
 
         progress = State.Proposed;
@@ -40,30 +47,30 @@ contract ArtCommission {
     }
 
     modifier onlyArtist() {
-        require(msg.sender == artist, "Not artist")
+        require(msg.sender == artist, "Not artist");
         _;
     }
 
     modifier onlyBuyer() {
-        require(msg.sender == buyer, "Not buyer") 
+        require(msg.sender == buyer, "Not buyer");
         _;
     }
 
     modifier onlyParties() {
-        require(msg.sender == buyer || msg.sender == artist, "Not involved party")
+        require(msg.sender == buyer || msg.sender == artist, "Not involved party");
         _;
     }
 
     //this confirms the parameters set in the constructor are ok with the other party
-    function artistConfirm() onlyArtist {
-        require(msg.value == insuranceAmount, "Insurance too low")
+    function artistConfirm() external payable onlyArtist {
+        require(msg.value == insuranceAmount, "Insurance too low");
         
         //set the state to confirmed
         progress = State.Confirmed;
     }
 
     //the artist submits work to the commission contract
-    function acceptArt(address nft, uint256 tokenID) onlyArtist {
+    function acceptArt(address nft, uint256 tokenID) external onlyArtist {
         artwork = IERC721(nft);
 
         require(artwork.ownerOf(tokenID) == artist, "Artist is not owner of the nft");
@@ -75,17 +82,17 @@ contract ArtCommission {
     }
 
     //the buyer pays for work, work and payment are released
-    function payInFullAndRelease() onlyBuyer {
+    function payInFullAndRelease() external onlyBuyer {
 
-        progress = State.Complete;
+        progress = State.Completed;
     }
 
     //update the trust score of buyer and artist
-    function updateTrustworthiness() {
+    function updateTrustworthiness() external {
 
     }
 
-    function raiseDispute() onlyParties {
+    function raiseDispute() external onlyParties {
 
         progress = State.Disputed;
     }
