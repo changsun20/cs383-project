b8e78b51f0d08c659fa8b8827231956868891ff7
TiaraSong <tiarasong00@gmail.com>
2026-04-11 23:18:56 -0400

    Add weighted NFT-holder jury selection and dispute voting flow

--- contracts/reputation.sol
+++ contracts/reputation.sol
@@ -1,11 +1,21 @@
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.26;
 
+/**
+ * @title Reputation
+ * @dev Stores a simple integer reputation score for each address.
+ * Positive values mean better reputation, negative values mean worse reputation.
+ */
 contract Reputation {
-    //reputation must be a integer which can be negative 
-    mapping(address => int256) public  reputation_score;
+    mapping(address => int256) public reputation_score;
 
+    /**
+     * @dev Adjust a user's reputation by delta.
+     * Example:
+     * - delta = +1 => increase reputation
+     * - delta = -1 => decrease reputation
+     */
     function adjust(address user, int256 delta) external {
         reputation_score[user] += delta;
     }
-
 }
