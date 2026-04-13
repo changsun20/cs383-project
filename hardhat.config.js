4a63a049fbd1c421bbbd1c10959707c39cd000f8
changsun20 <110759360+changsun20@users.noreply.github.com>
2026-04-13 16:19:52 -0400

    chore: remove script files

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,24 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: {
-    version: "0.8.26",//changed from 0.8.0
-    //added this to compile the NFT
-    settings: {
-      evmVersion: "cancun"
-    }
-  },
-  networks: {
-    hardhat: {
-      forking: {
-        url: "https://sepolia.infura.io/v3/" + String(process.env.API_KEY)
-      }
-    },
-    sepolia: {
-      url: String(process.env.SEPOLIA_RPC_URL),
-      accounts: [`0x${process.env.PRIVATE_KEY}`, `0x${process.env.PRIVATE_KEY_2}`]
-    }
-  }
+    solidity: "0.8.28",
 };
